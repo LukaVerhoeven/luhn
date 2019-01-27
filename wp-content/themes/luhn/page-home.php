2b1e469917e3a9ea2022d69ebe0d74d571bc72c4
luka <lka.v.lv@gmail.com>
2019-01-27 18:24:59 +0100

    Homepage-welcome block

--- wp-content/themes/luhn/page-home.php
+++ wp-content/themes/luhn/page-home.php
@@ -3,10 +3,9 @@
     
     <img id="logo" src="<?php bloginfo('template_directory'); ?>/dist/img/logo.gif" alt="Animation of the luhn logo">
 
-  <div class="row">
-    <h1 class="post-title"> <?php the_title();?> </h1>
-  </div>
   <?php 
+  $classList = ["welcome", "about", "skills", "contact"];
+  $counter = 0;
 
 /*
 *  Loop through a Flexible Content field and display it's content with different views for different layouts
@@ -14,19 +13,30 @@
 
 while(the_flexible_field("flexible")): ?>
 
-	<?php if(get_row_layout() == "title_with_text"): // layout: Content ?>
+	<?php if(get_row_layout() == "homepage_title_text"): // layout: Content ?>
 
-		<div>
-			<?php the_sub_field("title"); ?>
-            <!-- <?php the_sub_field("text"); ?> -->
+		<section class="home-section <?php echo $classList[$counter]?>">
+			<div class="home-content">
+				<h1>
+					<?php the_sub_field("title_intro"); ?>
+					<span class="title-big"><?php the_sub_field("title"); ?></span>
+				</h1>
+				<div class="ruler"></div>
+				<div class="ruler2"></div>
+				<p class="home-text"> <?php the_sub_field("text"); ?> </p>
+				
+			</div>
+			<div class="home-bg box<?php echo $counter?> clearfix">
+				<div class="shape1"></div>
+				<div class="shape2"></div>
+				<div class="shape3"></div>
+			</div>
+			<p class=user-text> <?php the_sub_field("user_text"); ?> </p>
+		</section>
+		<div class="divider-block">
+			<div class="graphic<?php echo $counter+1?>"></div>
+			<div class="vertical-ruler"></div>
 		</div>
-
-	<?php elseif(get_row_layout() == "file"): // layout: File ?>
-
-		<div>
-			<a href="<?php the_sub_field("file"); ?>" ><?php the_sub_field("name"); ?></a>
-		</div>
-
 	<?php elseif(get_row_layout() == "featured_posts"): // layout: Featured Posts ?>
 
 		<div>
@@ -44,7 +54,7 @@ while(the_flexible_field("flexible")): ?>
 		</div>
 
 	<?php endif; ?>
-
+	<?php $counter++?>
 <?php endwhile; ?>
 
 
